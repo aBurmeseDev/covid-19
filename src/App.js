589bdc3f1126f7bcbc84d948d427bf4bd7416c72
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2020-04-01 22:08:26 -0700

    get api and fetch data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Axios from 'axios'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Hello from the inside...
+class App extends Component {
+  state = {
+    confirmed: 100000,
+    recovered: 50000,
+    deaths: 7000
+  }
+  componentDidMount() {
+    this.getData()
+  }
+
+  async getData() {
+    const res = await Axios.get("https://covid19.mathdro.id/api");
+    this.setState({
+      confirmed: res.data.confirmed.value,
+      recovered: res.data.recovered.value,
+      deaths: res.data.deaths.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Hello from the inside...
       </header>
-    </div>
-  );
+      </div>
+    )
+  }
 }
+
 
 export default App;
