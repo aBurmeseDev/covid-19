6500816f53c3d1d67b57bf64e1fa3e998e4721a5
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2020-04-01 23:43:58 -0700

    fetch countries and convert into array

--- src/App.js
+++ src/App.js
@@ -7,18 +7,41 @@ class App extends Component {
   state = {
     confirmed: 100000,
     recovered: 50000,
-    deaths: 7000
+    deaths: 7000,
+    countries: []
   }
   componentDidMount() {
     this.getData()
   }
 
   async getData() {
-    const res = await Axios.get("https://covid19.mathdro.id/api");
+    const response = await Axios.get("https://covid19.mathdro.id/api");
+    const resLocation = await Axios.get("https://covid19.mathdro.id/api/countries")
+    // console.log(resLocation.data.countries)
+    let locationArr = resLocation.data.countries;
+    let countriesArr = []
+    for (let i = 0, l = locationArr.length; i < l; i++) {
+      countriesArr.push(locationArr[i].name)
+    }
+    console.log(countriesArr)
+    // console.log(countriesArr)
+    // const countries = Object.keys(resLocation.data.countries)
+
+    // function toObject(locationArr) {
+    //   var rv = {};
+    //   for (var i = 0; i < locationArr.length; ++i)
+    //     rv[i] = locationArr[i];
+    //   return rv;
+    //   console.log(rv)
+    // }
+    // toObject(locationArr);
+
+
     this.setState({
-      confirmed: res.data.confirmed.value,
-      recovered: res.data.recovered.value,
-      deaths: res.data.deaths.value
+      confirmed: response.data.confirmed.value,
+      recovered: response.data.recovered.value,
+      deaths: response.data.deaths.value,
+      countriesArr
     })
   }
 
@@ -26,15 +49,15 @@ class App extends Component {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-md">
-            <h3>Confirmed Cases</h3>
+          <div className="col py-3 px-lg-5 border bg-light">
+            <h3>Confirmed</h3>
             <h4>{this.state.confirmed}</h4>
           </div>
-          <div className="col-md">
+          <div className="col py-3 px-lg-5 border bg-light">
             <h3>Recovered</h3>
             <h4>{this.state.recovered}</h4>
           </div>
-          <div className="col-md">
+          <div className="col py-3 px-lg-5 border bg-light">
             <h3>Deaths</h3>
             <h4>{this.state.deaths}</h4>
           </div>
