713c36cf2fa257be25f87d800d48da62090cf34d
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2020-04-03 21:05:32 -0700

    move the video

--- src/App.js
+++ src/App.js
@@ -118,10 +118,7 @@ class App extends Component {
       <Container>
         <h5 className="text-center">Hello from the inside...</h5>
 
-        <Row style={{ marginTop: '2rem', borderTop: "2px solid #000" }}>
-          <Col xs={12} md={12} lg={12} style={{ paddingTop: '1rem' }}>
-            <h5 className="text-center">CoronaVirus Explained</h5>
-          </Col>
+        <Row style={{ marginTop: '2rem' }}>
           <Col>
             <select onChange={this.getCountryData} className="custom-select text-center" style={{ background: '#000', color: '#fff' }}>
               <option defaultValue>Select Country</option>
@@ -164,7 +161,10 @@ class App extends Component {
         </Row>
         {this.state.showUpdate ? <Alert variant="dark" style={{ marginTop: '1rem' }} className='text-center'> Last Updated: {this.state.update}</Alert> : null}
 
-        <Row>
+        <Row style={{ marginTop: '2rem', borderTop: "2px solid #000" }}>
+          <Col xs={12} md={12} lg={12} style={{ paddingTop: '1rem' }}>
+            <h5 className="text-center">CoronaVirus Explained</h5>
+          </Col>
           <Col xs={12} md={12} lg={12} style={{ paddingTop: '1rem' }}>
             <iframe title='covid explained' width="100%" height="315" src="https://www.youtube.com/embed/BtN-goy9VOY" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen='true'></iframe>
           </Col>
